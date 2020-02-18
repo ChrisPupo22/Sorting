8443909ce865a0d2a2d579ae49356b41cf20a124
Christopher Pupo <ChrisPupo@Christophers-MacBook-Pro.local>
2020-02-17 19:06:41 -0500

    cleaned up the solution code

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,15 +18,18 @@ def selection_sort(arr):
 print(selection_sort(arr))
 
 # TO-DO:  implement the Bubble Sort function below
+
+arr1 = [29, 0, 7, 2, 1, 3, 17]
 def bubble_sort( arr ):
-    isSorted = False
-    while isSorted: 
-        for b in range(0, len(arr) - 1): 
-            if arr[b] > arr[b + 1]: 
-                arr[b], arr[b + 1] = arr[b + 1], arr[b]
+    l = len(arr)
+    for b in range(l): 
+        for c in range(0, l-b-1):
+            if arr[c] > arr[c + 1]:
+                arr[c], arr[c + 1] = arr[c + 1], arr[c]
+               
     return arr
 
-print(bubble_sort(arr))
+print(bubble_sort(arr1))
 
 
 # STRETCH: implement the Count Sort function below
